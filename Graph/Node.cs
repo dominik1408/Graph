20b1c1a39bbc2201aa3a6a4916a84d07f5af17a3
dominik1408 <dominik.kasprzak@PK1-12>
2022-06-11 15:25:53 +0200

    Dodaj pliki projektów.

---
+++ Graph/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    class Node<T>
+    {
+        T value;
+
+        int number;
+
+        List<Edge> edges = new List<Edge>();
+    }
+}
